ad50493a05c970d637710d7144d6837a7d838f7a
Michael Jeffrey <michaeldjeffrey@gmail.com>
2022-11-02 15:22:39 -0700

    move cli def to it's own module

---
+++ src/cmds.rs
@@ -0,0 +1,108 @@
+use std::path::PathBuf;
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(name = "helium-config-cli")]
+#[command(author, version, about = "CLI for helium packet router config service", long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Commands,
+
+    /// Config file
+    #[arg(short, long, default_value = "./config/default.toml")]
+    pub config: PathBuf,
+}
+
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    /// Initialize your settings file
+    Init,
+    /// Org commands
+    Org {
+        #[command(subcommand)]
+        command: OrgCommands,
+    },
+
+    /// Route commands
+    Route {
+        #[command(subcommand)]
+        command: RouteCommands,
+    },
+
+    /// Output format for inserting Devaddr
+    Devaddr {
+        start_addr: String,
+        end_addr: String,
+        /// ID of route to apply devaddr range to (same as filename)
+        route: Option<String>,
+        /// Add the verified devaddr entry to the routes file
+        #[arg(short, long)]
+        commit: bool,
+    },
+    /// Output format for inserting EUI
+    Eui {
+        #[arg(short, long)]
+        dev_eui: String,
+        #[arg(short, long)]
+        app_eui: String,
+        /// ID of route to apply eui to (same as filename)
+        route: Option<String>,
+        /// Add the verified eui entry to the routes file
+        #[arg(short, long)]
+        commit: bool,
+    },
+}
+
+#[derive(Debug, Subcommand)]
+pub enum OrgCommands {
+    /// List Orgs
+    List,
+
+    /// View your Org (oui taken from default.toml)
+    Get,
+
+    /// Create an organization
+    Create {
+        #[arg(long)]
+        oui: u64,
+        #[arg(long)]
+        commit: bool,
+    },
+}
+
+#[derive(Debug, Subcommand)]
+pub enum RouteCommands {
+    /// List your Routes
+    List {
+        /// Write all routes to their own files locally
+        #[arg(short, long)]
+        commit: bool,
+    },
+    /// Get a Route
+    Get {
+        id: String,
+        /// Create or overwrite existing route with <ID>
+        #[arg(short, long)]
+        commit: bool,
+    },
+    /// Create a Route
+    Create {
+        /// Route will not be created without this flag
+        #[arg(short, long)]
+        commit: bool,
+    },
+    /// Delete a Route
+    Delete {
+        id: String,
+        /// Are you sure?
+        #[arg(short, long)]
+        commit: bool,
+    },
+    /// Push an updated route (note: nonce will auto-increment)
+    Push {
+        id: String,
+        /// Are you sure?
+        #[arg(short, long)]
+        commit: bool,
+    },
+}
--- src/main.rs
+++ src/main.rs
@@ -1,117 +1,13 @@
 mod client;
+mod cmds;
 mod settings;
 
-use crate::settings::Settings;
-use clap::{Parser, Subcommand};
+use clap::Parser;
+use cmds::{Cli, Commands, OrgCommands, RouteCommands};
 use helium_config_service_cli::{DevaddrRange, Eui, PrettyJson, Result, Route};
+use settings::Settings;
 use std::fs;
-use std::path::{Path, PathBuf};
-
-#[derive(Debug, Parser)]
-#[command(name = "helium-config-cli")]
-#[command(author, version, about = "CLI for helium packet router config service", long_about = None)]
-struct Cli {
-    #[command(subcommand)]
-    command: Commands,
-
-    /// Config file
-    #[arg(short, long, default_value = "./config/default.toml")]
-    config: PathBuf,
-}
-
-#[derive(Debug, Subcommand)]
-enum Commands {
-    /// Initialize your settings file
-    Init,
-    /// Org commands
-    Org {
-        #[command(subcommand)]
-        command: OrgCommands,
-    },
-
-    /// Route commands
-    Route {
-        #[command(subcommand)]
-        command: RouteCommands,
-    },
-
-    /// Output format for inserting Devaddr
-    Devaddr {
-        start_addr: String,
-        end_addr: String,
-        /// ID of route to apply devaddr range to (same as filename)
-        route: Option<String>,
-        /// Add the verified devaddr entry to the routes file
-        #[arg(short, long)]
-        commit: bool,
-    },
-    /// Output format for inserting EUI
-    Eui {
-        #[arg(short, long)]
-        dev_eui: String,
-        #[arg(short, long)]
-        app_eui: String,
-        /// ID of route to apply eui to (same as filename)
-        route: Option<String>,
-        /// Add the verified eui entry to the routes file
-        #[arg(short, long)]
-        commit: bool,
-    },
-}
-
-#[derive(Debug, Subcommand)]
-enum OrgCommands {
-    /// List Orgs
-    List,
-
-    /// View your Org (oui taken from default.toml)
-    Get,
-
-    /// Create an organization
-    Create {
-        #[arg(long)]
-        oui: u64,
-        #[arg(long)]
-        commit: bool,
-    },
-}
-
-#[derive(Debug, Subcommand)]
-enum RouteCommands {
-    /// List your Routes
-    List {
-        /// Write all routes to their own files locally
-        #[arg(short, long)]
-        commit: bool,
-    },
-    /// Get a Route
-    Get {
-        id: String,
-        /// Create or overwrite existing route with <ID>
-        #[arg(short, long)]
-        commit: bool,
-    },
-    /// Create a Route
-    Create {
-        /// Route will not be created without this flag
-        #[arg(short, long)]
-        commit: bool,
-    },
-    /// Delete a Route
-    Delete {
-        id: String,
-        /// Are you sure?
-        #[arg(short, long)]
-        commit: bool,
-    },
-    /// Push an updated route (note: nonce will auto-increment)
-    Push {
-        id: String,
-        /// Are you sure?
-        #[arg(short, long)]
-        commit: bool,
-    },
-}
+use std::path::Path;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -181,7 +77,7 @@ async fn main() -> Result<()> {
                 OrgCommands::Get => org_client.get(settings.oui).await?.print_pretty_json()?,
                 OrgCommands::Create { oui, commit } => {
                     let new_settings = settings.set_oui(oui);
-                    let dir = cli.config.parent().unwrap_or(Path::new("./config"));
+                    let dir = cli.config.parent().unwrap_or_else(|| Path::new("./config"));
 
                     match commit {
                         false => {
